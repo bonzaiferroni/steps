fbd28a1df83f3f7b8bb1453dcf8d9a70a93eb3f3
Luke Bollwerk <lukas8@gmail.com>
2025-05-24 11:32:36 -0600

    add kabinet and clutch as submodules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -31,9 +31,9 @@ dependencyResolutionManagement {
 include(":pondui")
 project(":pondui").projectDir = file("pondui/library")
 include(":kabinet")
-project(":kabinet").projectDir = file("../kabinet/library")
+project(":kabinet").projectDir = file("kabinet/library")
 include(":klutch")
-project(":klutch").projectDir = file("../klutch/library")
+project(":klutch").projectDir = file("klutch/library")
 
 include(":app")
 include(":model")
