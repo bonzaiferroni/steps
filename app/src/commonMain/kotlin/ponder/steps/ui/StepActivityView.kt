a8e2039b46bfd740e04d74a73ed9098e96ac9fa3
Luke Bollwerk <lukas8@gmail.com>
2025-06-22 14:37:30 -0600

    refactor chart args

--- app/src/commonMain/kotlin/ponder/steps/ui/StepActivityView.kt
+++ app/src/commonMain/kotlin/ponder/steps/ui/StepActivityView.kt
@@ -18,7 +18,6 @@ import pondui.ui.charts.AxisSide
 import pondui.ui.charts.BottomAxisAutoConfig
 import pondui.ui.charts.SideAxisAutoConfig
 import pondui.ui.charts.TimeChart
-import pondui.ui.charts.TimeChartArray
 import pondui.ui.theme.DefaultColors.swatches
 import pondui.ui.theme.Pond
 import kotlin.time.Duration.Companion.days
@@ -31,27 +30,26 @@ fun StepActivityView(stepId: String) {
     Column(1) {
         TimeChart(
             arrays = listOf(
-                TimeChartArray(
+                ChartArray(
                     values = state.buckets,
                     color = swatches[0],
                     provideY = { it.sum.toFloat() },
-                    provideX = { it.intervalStart },
                     axis = SideAxisAutoConfig(3, AxisSide.Left)
                 ),
-                TimeChartArray(
+                ChartArray(
                     values = state.buckets,
                     color = swatches[1],
                     provideY = { it.count.toFloat() },
-                    provideX = { it.intervalStart },
                     axis = SideAxisAutoConfig(3, AxisSide.Right)
                 )
             ),
             config = ChartConfig(
                 glowColor = Pond.colors.glow,
                 contentColor = Pond.localColors.content,
-                bottomAxis = BottomAxisAutoConfig(5)
+                bottomAxis = BottomAxisAutoConfig(5),
             ),
-            modifier = Modifier.fillMaxWidth().height(300.dp).background(Color.White.copy(.1f))
+            modifier = Modifier.fillMaxWidth().height(300.dp).background(Color.White.copy(.1f)),
+            provideX = { it.intervalStart },
         )
     }
 }
@@ -60,44 +58,3 @@ data class ChartValue(
     val x: Instant,
     val y: Float,
 )
-
-@Composable
-fun TimeChartExample() {
-    val now = Clock.System.now()
-    TimeChart(
-        arrays = listOf(
-            TimeChartArray(
-                values = listOf(
-                    ChartValue(now - 0.days, -10000f),
-                    ChartValue(now - 1.days, 1000f),
-                    ChartValue(now - 2.days, 5000f),
-                    ChartValue(now - 3.days, 3000f),
-                    ChartValue(now - 4.days, 5000f),
-                ),
-                color = swatches[0],
-                provideX = { it.x },
-                provideY = { it.y },
-                axis = SideAxisAutoConfig(3, AxisSide.Left)
-            ),
-            TimeChartArray(
-                values = listOf(
-                    ChartValue(now - 0.days, 30000f),
-                    ChartValue(now - 1.days, 15000f),
-                    ChartValue(now - 2.days, 7000f),
-                    ChartValue(now - 3.days, 15000f),
-                    ChartValue(now - 4.days, 8000f),
-                ),
-                color = swatches[1],
-                provideX = { it.x },
-                provideY = { it.y },
-                axis = SideAxisAutoConfig(3, AxisSide.Right)
-            )
-        ),
-        config = ChartConfig(
-            glowColor = Pond.colors.glow,
-            contentColor = Pond.localColors.content,
-            bottomAxis = BottomAxisAutoConfig(5)
-        ),
-        modifier = Modifier.fillMaxWidth().height(300.dp).background(Color.White.copy(.1f))
-    )
-}
